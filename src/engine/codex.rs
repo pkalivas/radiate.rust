72b5c068083e903f26312cfb7ba1f80b78829ced
Peter Kalivas <peterkalivas@gmail.com>
2024-06-15 21:09:50 -0500

    a

--- src/engine/codex.rs
+++ src/engine/codex.rs
@@ -1,10 +1,61 @@
 use crate::engine::genome::genes::gene::Gene;
-
-use super::genome::genotype::Genotype;
+use crate::engine::genome::genes::float_gene::FloatGene;
+use crate::engine::genome::genotype::Genotype;
+use crate::engine::genome::chromosome::Chromosome;
+use crate::engine::genome::genes::gene::Allele;
 
 pub trait Codex<TGene, T>
     where TGene : Gene<TGene>
 {
     fn encode(&self) -> Genotype<TGene>;
-    fn decode(&self, genotype: Genotype<TGene>) -> T;
+    fn decode(&self, genotype: &Genotype<TGene>) -> T;
+}
+
+
+pub struct FloatCodex {
+    pub num_chromosomes: usize,
+    pub num_genes: usize
+}
+
+impl FloatCodex {
+    pub fn new(num_chromosomes: usize, num_genes: usize) -> Self {
+        FloatCodex {
+            num_chromosomes,
+            num_genes,
+        }
+    }
+}
+
+impl Codex<FloatGene, Vec<Vec<f32>>> for FloatCodex {
+    fn encode(&self) -> Genotype<FloatGene> {
+        let mut chromosomes = Vec::with_capacity(self.num_chromosomes);
+
+        for _ in 0..self.num_chromosomes {
+            let mut genes = Vec::with_capacity(self.num_genes);
+
+            for _ in 0..self.num_genes {
+                genes.push(FloatGene::new());
+            }
+
+            chromosomes.push(Chromosome::from_vec(genes));
+        }
+
+        Genotype::from_slice(&chromosomes)
+    }
+
+    fn decode(&self, genotype: &Genotype<FloatGene>) -> Vec<Vec<f32>> {
+        let mut decoded = Vec::with_capacity(self.num_chromosomes);
+
+        for chromosome in &genotype.chromosomes {
+            let mut genes = Vec::with_capacity(self.num_genes);
+
+            for gene in &chromosome.genes {
+                genes.push(*gene.allele());
+            }
+
+            decoded.push(genes);
+        }
+
+        decoded
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod engine;
 
+use engine::codex::Codex;
 use engine::genome::genotype::Genotype;
 use engine::genome::genes::float_gene::FloatGene;
 use engine::genome::genes::char_gene::CharGene;
@@ -8,18 +9,26 @@ use engine::genome::phenotype::Phenotype;
 use engine::genome::population::Population;
 
 fn main() {
-    let float_genotype = create_float_genotype(3, 2);
-    let char_genotype = create_char_genotype(3, 2);
+    // let float_genotype = create_float_genotype(3, 2);
+    // let char_genotype = create_char_genotype(3, 2);
 
-    println!("{:?}", float_genotype);
-    println!("{:?}", char_genotype);
+    // println!("{:?}", float_genotype);
+    // println!("{:?}", char_genotype);
 
-    let population = Population::from_func(10, || {
-        let genotype = create_char_genotype(10, 1);
-        return Phenotype::from_genotype(genotype);
-    });
+    // let population = Population::from_func(10, || {
+    //     let genotype = create_char_genotype(10, 1);
+    //     return Phenotype::from_genotype(genotype);
+    // });
 
-    println!("{:?}", population);
+    // println!("{:?}", population);
+
+    let codex = engine::codex::FloatCodex::new(3, 2);
+
+    let encoded_genotype = codex.encode();
+    let decoded_genotype = codex.decode(&encoded_genotype);
+
+    println!("{:?}", encoded_genotype);
+    println!("{:?}", decoded_genotype);
 }
 
 fn create_char_genotype(gene_count: i32, chromosome_count: i32) -> Genotype<CharGene> {
