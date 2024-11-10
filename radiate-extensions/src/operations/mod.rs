ecfca5ddb4206394eb643f7eb85436bef05231cb
Peter Kalivas <peterkalivas@gmail.com>
2024-11-10 13:04:54 -0600

    operations

--- radiate-extensions/src/operations/mod.rs
+++ radiate-extensions/src/operations/mod.rs
@@ -1,4 +1,2 @@
 pub mod op;
-pub mod math_op;
-pub mod mutable_const_op;
-pub mod var_op;
+pub mod operation;
--- radiate-extensions/src/operations/op.rs
+++ radiate-extensions/src/operations/op.rs
@@ -2,11 +2,9 @@ use std::ops::{Add, Div, Mul, Sub};
 
 use rand::{prelude::Distribution, random};
 
-use crate::operations::math_op::MathOp;
-use crate::operations::var_op::VarOp;
-use crate::operations::mutable_const_op::MutableConstOp;
+use super::operation::Operation;
 
-pub trait Op<T> {
+pub trait Op<T>  {
     fn name(&self) -> &str;
     fn arity(&self) -> u8;
     fn apply(&self, inputs: &[T]) -> T;
@@ -18,65 +16,50 @@ impl<T> std::fmt::Display for dyn Op<T> {
     }
 }
 
-impl<T, F> Op<T> for F where F: Fn(&[T]) -> T {
-    fn name(&self) -> &str {
-        "Op"
-    }
-
-    fn arity(&self) -> u8 {
-        0
-    }
-
-    fn apply(&self, inputs: &[T]) -> T {
-        (self)(inputs)
-    }
-}
-
-
 pub fn add<T>() -> impl Op<T> 
 where
-    T: Add<Output = T> + Copy
+    T: Add<Output = T> + Clone
 {
-    MathOp::Add(2, Box::new(|inputs: &[T]| inputs[0] + inputs[1]))
+    Operation::Math("+", 2, Box::new(|inputs: &[T]| inputs[0].clone() + inputs[1].clone()))
 }
 
 pub fn sub<T>() -> impl Op<T> 
 where
-    T: Sub<Output = T> + Copy
+    T: Sub<Output = T> + Clone
 {
-    MathOp::Subtract(2, Box::new(|inputs: &[T]| inputs[0] - inputs[1]))
+    Operation::Math("-", 2, Box::new(|inputs: &[T]| inputs[0].clone() - inputs[1].clone()))
 }
 
 pub fn mul<T>() -> impl Op<T> 
 where
-    T: Mul<Output = T> + Copy
+    T: Mul<Output = T> + Clone
 {
-    MathOp::Multiply(2, Box::new(|inputs: &[T]| inputs[0] * inputs[1]))
+    Operation::Math("*", 2, Box::new(|inputs: &[T]| inputs[0].clone() * inputs[1].clone()))
 }
 
 pub fn div<T>() -> impl Op<T> 
 where
-    T: Div<Output = T> + Copy
+    T: Div<Output = T> + Clone
 {
-    MathOp::Divide(2, Box::new(|inputs: &[T]| inputs[0] / inputs[1]))
+    Operation::Math("/", 2, Box::new(|inputs: &[T]| inputs[0].clone() / inputs[1].clone()))
 }
 
 pub fn sum<T>() -> impl Op<T> 
 where
-    T: Add<Output = T> + Copy + Default
+    T: Add<Output = T> + Clone + Default
 {
-    MathOp::Sum(2, Box::new(|inputs: &[T]| inputs
+    Operation::Math("sum", 2, Box::new(|inputs: &[T]| inputs
         .iter()
-        .fold(T::default(), |acc, &x| acc + x)))
+        .fold(T::default(), |acc, x| acc + x.clone())))
 }
 
 pub fn prod<T>() -> impl Op<T> 
 where
-    T: Mul<Output = T> + Copy + Default
+    T: Mul<Output = T> + Clone + Default
 {
-    MathOp::Prod(2, Box::new(|inputs: &[T]| inputs
+    Operation::Math("prod", 2, Box::new(|inputs: &[T]| inputs
         .iter()
-        .fold(T::default(), |acc, &x| acc * x)))
+        .fold(T::default(), |acc, x| acc * x.clone())))
 }
 
 pub fn weight<T>() -> impl Op<T> 
@@ -86,7 +69,7 @@ where
 {
     let supplier = || random::<T>() - random::<T>();
     let operation = |inputs: &[T], weight: &T| inputs[0] * *weight;
-    MutableConstOp::new("w", 1, Box::new(supplier), Box::new(operation))
+    Operation::MutableConst("w", 1, supplier(), Box::new(supplier), Box::new(operation))
 }
 
 pub fn var<T>(index: usize) -> impl Op<T> 
@@ -94,5 +77,5 @@ where
     T: Clone
 {
     let var_name = format!("x{}", index);
-    VarOp::new(var_name, index)
+    Operation::Var(var_name, index)
 }
