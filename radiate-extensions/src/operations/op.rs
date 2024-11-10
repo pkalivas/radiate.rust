233d1e6b6a923dc3c408432f30e6a1dcffface2e
Peter Kalivas <peterkalivas@gmail.com>
2024-11-10 13:54:46 -0600

    ops

--- radiate-extensions/src/operations/op.rs
+++ radiate-extensions/src/operations/op.rs
@@ -1,6 +1,6 @@
-use std::{ops::{Add, Div, Mul, Sub}, sync::Arc};
+use std::{ops::{Add, Div, Mul, Sub, Neg}, sync::Arc};
 
-use rand::{prelude::Distribution, random};
+use rand::{prelude::Distribution, distributions::Standard, random};
 
 use super::ops::Ops;
 
@@ -8,73 +8,48 @@ pub fn add<T: Add<Output = T> + Clone>() -> Ops<T> {
     Ops::Math("+", 2, Arc::new(|inputs: &[T]| inputs[0].clone() + inputs[1].clone()))
 }
 
-pub fn sub<T>() -> Ops<T>
-where
-    T: Sub<Output = T> + Clone
-{
+pub fn sub<T: Sub<Output = T> + Clone>() -> Ops<T> {
     Ops::Math("-", 2, Arc::new(|inputs: &[T]| inputs[0].clone() - inputs[1].clone()))
 }
 
-pub fn mul<T>() -> Ops<T>
-where
-    T: Mul<Output = T> + Clone
-{
+pub fn mul<T: Mul<Output = T> + Clone>() -> Ops<T> {
     Ops::Math("*", 2, Arc::new(|inputs: &[T]| inputs[0].clone() * inputs[1].clone()))
 }
 
-pub fn div<T>() -> Ops<T>
-where
-    T: Div<Output = T> + Clone
-{
+pub fn div<T: Div<Output = T> + Clone>() -> Ops<T> {
     Ops::Math("/", 2, Arc::new(|inputs: &[T]| inputs[0].clone() / inputs[1].clone()))
 }
 
-pub fn sum<T>() -> Ops<T>
-where
-    T: Add<Output = T> + Clone + Default
-{
+pub fn sum<T: Add<Output = T> + Clone + Default>() -> Ops<T> {
     Ops::Math("sum", 2, Arc::new(|inputs: &[T]| inputs
         .iter()
         .fold(T::default(), |acc, x| acc + x.clone())))
 }
 
-pub fn prod<T>() -> Ops<T>
-where
-    T: Mul<Output = T> + Clone + Default
-{
+pub fn prod<T: Mul<Output = T> + Clone + Default>() -> Ops<T> {
     Ops::Math("prod", 2, Arc::new(|inputs: &[T]| inputs
         .iter()
         .fold(T::default(), |acc, x| acc * x.clone())))
 }
 
-pub fn neg<T>() -> Ops<T>
-where
-    T: std::ops::Neg<Output = T> + Clone + Default
-{
+pub fn neg<T: Neg<Output = T> + Clone + Default>() -> Ops<T> {
     Ops::Math("neg", 1, Arc::new(|inputs: &[T]| -inputs[0].clone()))
 }
 
-pub fn pow<T>() -> Ops<T>
-where
-    T: Mul<Output = T> + Clone
-{
+pub fn pow<T: Mul<Output = T> + Clone>() -> Ops<T> {
     Ops::Math("pow", 2, Arc::new(|inputs: &[T]| inputs[0].clone() * inputs[1].clone()))
 }
 
-pub fn weight<T>() -> Ops<T>
+pub fn weight<T: Sub<Output = T> + Mul<Output = T> + Copy + Default>() -> Ops<T>
 where
-    rand::distributions::Standard: Distribution<T>,
-    T: Sub<Output = T> + Mul<Output = T> + Copy + Default
+    Standard: Distribution<T>,
 {
     let supplier = || random::<T>() - random::<T>();
     let operation = |inputs: &[T], weight: &T| inputs[0] * *weight;
     Ops::MutableConst("w", 1, supplier(), Arc::new(supplier), Arc::new(operation))
 }
 
-pub fn var<T>(index: usize) -> Ops<T>
-where
-    T: Clone
-{
+pub fn var<T: Clone>(index: usize) -> Ops<T> {
     let var_name = format!("x{}", index);
     Ops::Var(var_name, index)
 }
