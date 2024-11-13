18fce8136fca157d51e8630918c88f31a4506b21
Peter Kalivas <peterkalivas@gmail.com>
2024-11-12 23:28:53 -0600

    adf

--- radiate-extensions/src/architects/mod.rs
+++ radiate-extensions/src/architects/mod.rs
@@ -3,3 +3,6 @@ pub mod architect;
 pub mod schema;
 pub mod node_collection_builder;
 pub mod node_collections;
+
+pub use architect::Architect;
+pub use node_collection_builder::NodeCollectionBuilder;
--- radiate-extensions/src/architects/node_collections/graphs/mod.rs
+++ radiate-extensions/src/architects/node_collections/graphs/mod.rs
@@ -1,3 +1,7 @@
 pub mod graph_codex;
 pub mod graph;
 pub mod graph_reducer;
+
+pub use graph_codex::GraphCodex;
+pub use graph::Graph;
+pub use graph_reducer::GraphReducer;
