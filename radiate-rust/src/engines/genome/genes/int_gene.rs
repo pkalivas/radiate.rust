a3b03ad904aabb2a5275936cda15a9a53a351e0b
Peter Kalivas <peterkalivas@gmail.com>
2024-11-15 21:00:43 -0600

    asdf

--- radiate-rust/src/engines/genome/genes/int_gene.rs
+++ radiate-rust/src/engines/genome/genes/int_gene.rs
@@ -1,17 +1,20 @@
 use rand::Rng;
 
-use super::gene::{BoundGene, Gene, NumericGene, Valid};
+use super::{
+    gene::{BoundGene, Gene, NumericGene, Valid},
+    Integer,
+};
 
-pub struct IntGene {
-    allele: i32,
-    min: i32,
-    max: i32,
-    upper_bound: i32,
-    lower_bound: i32,
+pub struct IntGene<T: Integer<T>> {
+    allele: T,
+    min: T,
+    max: T,
+    upper_bound: T,
+    lower_bound: T,
 }
 
-impl IntGene {
-    pub fn new(min: i32, max: i32) -> Self {
+impl<T: Integer<T>> IntGene<T> {
+    pub fn new(min: T, max: T) -> Self {
         let (min, max) = if min > max { (max, min) } else { (min, max) };
         let mut rand = rand::thread_rng();
 
@@ -19,18 +22,18 @@ impl IntGene {
             allele: rand.gen_range(min..max),
             min,
             max,
-            upper_bound: i32::MAX,
-            lower_bound: i32::MIN,
+            upper_bound: T::MAX,
+            lower_bound: T::MIN,
         }
     }
 }
 
-impl Gene<IntGene, i32> for IntGene {
-    fn allele(&self) -> &i32 {
+impl<T: Integer<T>> Gene<IntGene<T>, T> for IntGene<T> {
+    fn allele(&self) -> &T {
         &self.allele
     }
 
-    fn new_instance(&self) -> IntGene {
+    fn new_instance(&self) -> IntGene<T> {
         let mut rand = rand::thread_rng();
         IntGene {
             allele: rand.gen_range(self.min..self.max),
@@ -41,7 +44,7 @@ impl Gene<IntGene, i32> for IntGene {
         }
     }
 
-    fn from_allele(&self, allele: &i32) -> IntGene {
+    fn from_allele(&self, allele: &T) -> IntGene<T> {
         IntGene {
             allele: *allele,
             min: self.min,
@@ -52,22 +55,22 @@ impl Gene<IntGene, i32> for IntGene {
     }
 }
 
-impl Valid for IntGene {
+impl<T: Integer<T>> Valid for IntGene<T> {
     fn is_valid(&self) -> bool {
         self.allele >= self.min && self.allele <= self.max
     }
 }
 
-impl BoundGene<IntGene, i32> for IntGene {
-    fn upper_bound(&self) -> &i32 {
+impl<T: Integer<T>> BoundGene<IntGene<T>, T> for IntGene<T> {
+    fn upper_bound(&self) -> &T {
         &self.upper_bound
     }
 
-    fn lower_bound(&self) -> &i32 {
+    fn lower_bound(&self) -> &T {
         &self.lower_bound
     }
 
-    fn with_bounds(self, upper_bound: i32, lower_bound: i32) -> IntGene {
+    fn with_bounds(self, upper_bound: T, lower_bound: T) -> IntGene<T> {
         IntGene {
             upper_bound,
             lower_bound,
@@ -76,31 +79,31 @@ impl BoundGene<IntGene, i32> for IntGene {
     }
 }
 
-impl NumericGene<IntGene, i32> for IntGene {
-    fn add(&self, other: &IntGene) -> IntGene {
+impl<T: Integer<T>> NumericGene<IntGene<T>, T> for IntGene<T> {
+    fn add(&self, other: &IntGene<T>) -> IntGene<T> {
         IntGene {
             allele: self.allele + other.allele,
             ..*self
         }
     }
 
-    fn sub(&self, other: &IntGene) -> IntGene {
+    fn sub(&self, other: &IntGene<T>) -> IntGene<T> {
         IntGene {
             allele: self.allele - other.allele,
             ..*self
         }
     }
 
-    fn mul(&self, other: &IntGene) -> IntGene {
+    fn mul(&self, other: &IntGene<T>) -> IntGene<T> {
         IntGene {
             allele: self.allele * other.allele,
             ..*self
         }
     }
 
-    fn div(&self, other: &IntGene) -> IntGene {
-        let other_allele = match other.allele() == &0_i32 {
-            true => 1_i32,
+    fn div(&self, other: &IntGene<T>) -> IntGene<T> {
+        let other_allele = match other.allele() == &T::from_i32(0) {
+            true => T::from_i32(1),
             false => *other.allele(),
         };
 
@@ -110,15 +113,15 @@ impl NumericGene<IntGene, i32> for IntGene {
         }
     }
 
-    fn mean(&self, other: &IntGene) -> IntGene {
+    fn mean(&self, other: &IntGene<T>) -> IntGene<T> {
         IntGene {
-            allele: (self.allele + other.allele) / 2,
+            allele: (self.allele + other.allele) / T::from_i32(2),
             ..*self
         }
     }
 }
 
-impl Clone for IntGene {
+impl<T: Integer<T>> Clone for IntGene<T> {
     fn clone(&self) -> Self {
         IntGene {
             allele: self.allele,
@@ -130,13 +133,13 @@ impl Clone for IntGene {
     }
 }
 
-impl PartialEq for IntGene {
+impl<T: Integer<T>> PartialEq for IntGene<T> {
     fn eq(&self, other: &Self) -> bool {
         self.allele == other.allele
     }
 }
 
-impl std::fmt::Debug for IntGene {
+impl<T: Integer<T>> std::fmt::Debug for IntGene<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.allele)
     }
